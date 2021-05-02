bfc357d932ce01d3e17ae287d95b011ed3ff8d5f
Mauricio Ferrite <mauricio.ferrite@gmail.com>
2021-05-02 14:22:56 +0100

    Condicional Switch

---
+++ revisao/condicional-switch.js
@@ -0,0 +1,28 @@
+let dia = 4;
+
+switch (dia) {
+    case 0:
+        dia = "domingo";
+        break
+    case 1:
+        dia = "segunda";
+        break
+    case 2:
+        dia = "terça";
+        break
+    case 3:
+        dia = "quarta";
+        break
+    case 4:
+        dia = "quinta";
+        break
+    case 5:
+        dia = "sexta";
+        break
+    case 6:
+        dia = "sábado";
+    default:
+        dia = " -- " 
+}
+
+console.log( dia )
